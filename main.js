f2ef7ad5c615bcc332a7e382be59b9eaa1913862
Raiane <raiane.oliveira404@gmail.com>
2023-02-08 12:36:02 -0300

    fix: quebra do layout na hora de fechar a sidebar

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 const menuVetor = document.querySelector(".menu")
-const divsNav = document.querySelectorAll(".menu-nav div")
+const optionsMenu = document.querySelectorAll(".menu-nav div")
 const aside = document.querySelector("aside")
 const logoVet = document.querySelector(".logo-vet")
 const spansSidebar = document.querySelectorAll(".menu-nav div span")
 
-const informationUser = document.querySelector(".information-user");
+const imgAvatar = document.querySelector(".logout img")
+const informationUser = document.querySelector(".information-user")
 
+logoVet.style.display = "none"
 let isSidebarOpen = false;
-menuVetor.addEventListener("click", openSidebar)
+// menuVetor.addEventListener("click", openSidebar)
+
+optionsMenu.forEach((option) => {
+    console.log(option)
+    option.addEventListener("click", openSidebar)
+})
 
 function openSidebar() {
     aside.style.width = aside.style.width === "250px" ? "78px" : "250px"
     isSidebarOpen = !isSidebarOpen
-        
-    logoVet.classList.toggle("invisible")
+    
+    logoVet.style.display = logoVet.style.display === "none" ? "block" : 'none'
     spansSidebar.forEach(span => {
         span.classList.toggle("invisible")
     })
-    
+    imgAvatar.classList.toggle("invisible")
     informationUser.classList.toggle("invisible")
-    if (!informationUser.className.includes("invisible")) {
-        informationUser.style.display = "flex"
-    } else {
-        informationUser.style.display = "none"
-    }
 }
